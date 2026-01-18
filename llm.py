f4d60b9461b78e1377022190b3c525bdae0c6c00
Shyam_Kumar_44 <93427182+ShyamKumar-AI-DS@users.noreply.github.com>
2026-01-18 12:28:34 +0530

    commit
    Removed the ChatGroq instantiation and kept the LLM instantiation.

--- llm.py
+++ llm.py
@@ -4,23 +4,9 @@ from crewai import LLM
 
 load_dotenv()
 
-from langchain_groq import ChatGroq
-
-<<<<<<< HEAD
 llms = LLM(
     model= "groq/openai/gpt-oss-20b",
     api_key=os.getenv("GROQ_API_KEY"),
     temperature=0.3,
     max_tokens = 1000,
 )
-=======
-# instantiate the LangChain model
-def llms():
-    return ChatGroq(
-        model="openai/gpt-oss-120b",
-        temperature=0.3,
-        max_tokens=512,
-        groq_api_key=os.getenv("GROQ_API_KEY")
-    )
-
->>>>>>> 92a26d881a969db236ec41a7bdd0aa2d402f526b
