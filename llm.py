39d5e64b83473121ed4424c20f2fa289bda36364
Shyam_Kumar_44 <93427182+ShyamKumar-AI-DS@users.noreply.github.com>
2026-01-18 13:17:45 +0530

    commit

--- llm.py
+++ llm.py
@@ -1,12 +1,12 @@
 import os
 from dotenv import load_dotenv
-from crewai import LLM
+from langchain_groq import ChatGroq
 
 load_dotenv()
 
-llms = LLM(
-    model= "groq/openai/gpt-oss-20b",
+llms = ChatGroq(
+    model="groq/openai/gpt-oss-20b",   # or llama3-70b-8192
     api_key=os.getenv("GROQ_API_KEY"),
     temperature=0.3,
-    max_tokens = 1000,
+    max_tokens=1000,
 )
